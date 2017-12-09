05a71a2f00d988eebc5e5ef4dfd214124bdf9eeb
arup rakshit <aruprakshit@rocketmail.com>
2017-12-09 13:34:49 +0530

    test error boundary

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ErrorApp from './ErrorApp';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<ErrorApp />, document.getElementById('root'));
+
+// registerServiceWorker();
